2f25014d2d2a29c4386f8f262da72da3ddda2aae
Стукалов Евгений Александрович <sea@mcxxi.local>
2017-09-01 14:50:55 +0300

    second try

--- redirect.php
+++ redirect.php
@@ -3,8 +3,8 @@
 		
 		if (isset($_FILES['myfile']['name']) && !empty($_FILES['myfile']['name']))	{
 			
-			if (is_dir($dir) && $_FILES['myfile']['error']==UPLOAD_ERR_OK && move_uploaded_file($_FILES['myfile']['tmp_name'], 'test/' . $_FILES['myfile']['name']) && 
-				isset($_FILES['myfile']['type']) && ($_FILES['myfile']['type']=='application/json')) {			
+			if (is_dir($dir) && $_FILES['myfile']['error']==UPLOAD_ERR_OK && isset($_FILES['myfile']['type']) && ($_FILES['myfile']['type']=='application/json') && 
+				move_uploaded_file($_FILES['myfile']['tmp_name'], 'test/' . $_FILES['myfile']['name'])) {			
 				header('Location: list.php');
 			}
 			else {
