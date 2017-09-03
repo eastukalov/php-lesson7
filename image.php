37b8ff78096d8ed1ac6c4567f8e02331b10e2869
eastukalov <evg1075@yandex.ru>
2017-09-03 12:16:19 +0300

    second try

--- image.php
+++ image.php
@@ -3,7 +3,7 @@
     $text_color = imagecolorallocate($im, 19, 136, 8);
     $back_color = imagecolorallocate($im, 220, 220, 220);
     imagefill($im, 0, 0, $back_color);
-    $font = __DIR__ . '\\font\\' . 'ofont.ru_Microsoft Sans Serif.ttf';
+    $font = __DIR__ . '/font/' . 'ofont.ru_Microsoft Sans Serif.ttf';
     $text=$_GET['fio'] . ', Ваш результат: ' . $_GET['score'];
     imagettftext($im, 45, 0, 10, 70, $text_color, $font, $text);
    	header('Content-Type: image/png');
